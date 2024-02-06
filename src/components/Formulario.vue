3d8541267f2553819068f9c102b12a169ff789b2
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-02-05 22:33:04 -0300

    add: Course 2 - chapter 2 done

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,49 +1,65 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input
-                    type="text" 
-                    class="input" 
-                    placeholder="Qual tarefa você deseja iniciar?"
-                    v-model="descricao"
-                >
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+                            {{ projeto.nome }}
+                        </option>
+                    </select>
+                </div>
             </div>
             <div class="column">
-                <Temporizador @ao-temporizador-finalizado="finalizarTarefa"/>
+                <Temporizador @ao-temporizador-finalizado="finalizarTarefa" />
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import Temporizador from './Temporizador.vue'
- 
-    export default defineComponent({
+import { computed, defineComponent } from 'vue'
+import Temporizador from './Temporizador.vue'
+import { useStore } from 'vuex'
+import { key } from '@/store'
+
+export default defineComponent({
     name: 'Formulario',
     components: { Temporizador },
+    emits: ['aoSalvarTarefa'],
     data() {
         return {
-            descricao: ''
+            descricao: '',
+            idProjeto: ''
         }
     },
     methods: {
-        finalizarTarefa(tempoDecorrido: number) : void {
+        finalizarTarefa(tempoDecorrido: number): void {
             this.$emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
             })
             this.descricao = ''
         }
     },
-    emits: ['aoSalvarTarefa']
+    setup() {
+        const store = useStore(key)
+
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
 })
 </script>
 
 <style>
-    .formulario {
-        color: var(--texto-primario);
-        background-color: var(--bg-primario);
-    }
+.formulario {
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+}
 </style>
